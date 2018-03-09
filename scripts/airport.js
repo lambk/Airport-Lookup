4828e8f0520d1bc7d3cb216b2dfee182a84b18c3
Kyle Lamb <kyle@Kyles-MBP.fritz.box>
2018-03-09 23:39:18 +1300

    Google map now loads appropriate location. Recenter button now functions and resets lat long and zoom to current airport. Info fields now have placeholder Loading... text while api is called. Temporary fetch button removed and api is now called on page load

--- scripts/airport.js
+++ scripts/airport.js
@@ -1,55 +1,71 @@
 $(function () {
   const icao = window.location.pathname.slice(9, 14).toUpperCase();
-  $('#fetch').click(function() {
-    getAirportData(icao);
-    getMetar(icao);
-  })
-  //getAirportData(icao);
-  //getMetar(icao);
-  //-37.010516,174.787814
-
-  function getAirportData(icao) {
-    $.ajax({
-      type: 'GET',
-      url: '/api/airport/' + icao,
-      contentType: 'application/json',
-      success: function(data) {
-        loadAirportData(data);
-      },
-      error: function(data) {
-        console.log('Error with Airport Data call')
-      }
-    });
-  }
-
-  function getMetar(icao) {
-    $.ajax({
-      type: 'GET',
-      url: '/api/metar/' + icao,
-      contentType: 'application/json',
-      success: function(data) {
-        loadMetar(data);
-      },
-      error: function(data) {
-        console.log('Error with Metar call');
-      }
-    });
-  }
-
-  function loadAirportData(data) {
-    var airport = data.data[0];
-    console.log(airport);
-    $('#title').html(airport.icao + ' - ' + airport.name);
-    $('#latitude').html(airport.latitude.decimal);
-    $('#longitude').html(airport.longitude.decimal);
-    $('#city').html(airport.city);
-    $('#country').html(airport.country);
-    $('#timezone').html(airport.timezone.tzid);
-    $('#status').html(airport.status);
-  }
-
-  function loadMetar(data) {
-    var metar = data.data[0];
-    $('#metar').html(metar);
-  }
+  getAirportData(icao);
+  getMetar(icao);
 });
+
+
+function getAirportData(icao) {
+  $.ajax({
+    type: 'GET',
+    url: '/api/airport/' + icao,
+    contentType: 'application/json',
+    success: function(data) {
+      let airport = data.data[0];
+      loadAirportData(airport);
+      loadMap(airport.latitude.decimal, airport.longitude.decimal);
+    },
+    error: function(data) {
+      console.log('Error with Airport Data call');
+    }
+  });
+}
+
+function getMetar(icao) {
+  $.ajax({
+    type: 'GET',
+    url: '/api/metar/' + icao,
+    contentType: 'application/json',
+    success: function(data) {
+      let metar = data.data[0];
+      loadMetar(metar)
+    },
+    error: function(data) {
+      console.log('Error with Metar call');
+    }
+  });
+}
+
+function loadAirportData(airport) {
+  $('#title').html(airport.icao + ' - ' + airport.name);
+  $('#latitude').html(airport.latitude.decimal);
+  $('#longitude').html(airport.longitude.decimal);
+  $('#city').html(airport.city);
+  $('#country').html(airport.country);
+  $('#timezone').html(airport.timezone.tzid);
+  $('#status').html(airport.status);
+}
+
+function loadMap(lat, long) {
+  var mapLocation = new google.maps.LatLng(lat, long);
+  var mapOptions = {
+    center: mapLocation,
+    zoom: 13,
+    mapTypeId: 'satellite'
+  };
+
+  var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+
+  setRecenterEventHandler(map, lat, long);
+}
+
+function setRecenterEventHandler(map, lat, long) {
+  $('#recenterBtn').click(function () {
+    map.setCenter(new google.maps.LatLng(lat, long));
+    map.setZoom(13);
+  });
+}
+
+function loadMetar(metar) {
+  $('#metar').html(metar);
+}
