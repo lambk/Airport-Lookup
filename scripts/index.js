f4e5e2c8770a307017d18672c01f99ac797d3ac9
lambk <kyle@Kyles-MBP.fritz.box>
2018-03-10 20:45:26 +1300

    Hitting enter while the ICAO input has focus now triggers the search

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,31 @@
 $(function() {
+  //Clear the 'Field required' tooltip when the user types or changes the ICAO input (if the input isn't still empty)
   $('#icaoTxt').on('change keyup', function() {
     if ($(this).val().length > 0) {
       $('#icaoTT').addClass('invisible');
     }
   });
 
-  $('#searchBtn').click(function() {
-    icao = $('#icaoTxt').val().trim();
-    if (icao.length == 0) {
-      $('#icaoTT').removeClass('invisible');
-    } else {
-      window.location = '/airport/' + icao;
+  //Call the search function if the user hits the Enter key while the ICAO input has focus
+  $('#icaoTxt').keyup(function(event) {
+    if (event.keyCode == 13) {
+      search();
     }
-  });
+  })
+
+  //Call the search function when the search button is clicked
+  $('#searchBtn').click(search);
 });
+
+/*
+  Checks whether the ICAO input is empty (or only consisting of whitespace). If so, the 'Field required' tooltip
+  is shown. If not, the page is redirected to /airport/<icao>
+*/
+function search() {
+  icao = $('#icaoTxt').val().trim();
+  if (icao.length == 0) {
+    $('#icaoTT').removeClass('invisible');
+  } else {
+    window.location = '/airport/' + icao;
+  }
+}
