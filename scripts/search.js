6e71f100f4b6cab1d02d7b2a3d786c4904c3024c
lambk <kyle.lamb@hotmail.co.nz>
2018-03-24 21:05:47 +1300

    Added validation for incorrect icao formats. Thus the page can no longer redirect itself to invalid enpoints if the icao contains a forward slash.

--- scripts/search.js
+++ scripts/search.js
@@ -23,12 +23,16 @@ $(function() {
 });
 
 /*
-  Checks whether the ICAO input is empty (or only consisting of whitespace). If so, the 'Field required' tooltip
-  is shown. If not, the page is redirected to /airport/<icao>
+  Checks whether the ICAO input is either empty (or only consisting of whitespace) or not in valid ICAO format (4 letters only).
+  If so, the tooltip is shown with appropriate feedback. If not, the page is redirected to /airport/<icao>
 */
 function search() {
   icao = $('#icaoTxt').val().trim();
   if (icao.length == 0) {
+    $('.tooltip-container').html('Field required');
+    $('#icaoTT').removeClass('invisible');
+  } else if (!/^[A-Z|a-z]{4}$/.test(icao)) {
+    $('.tooltip-container').html('Not a valid ICAO code');
     $('#icaoTT').removeClass('invisible');
   } else {
     window.location = '/airport/' + icao.toLowerCase();
