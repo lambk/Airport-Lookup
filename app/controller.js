55dfbc1c2f6a4409866049dd2e7d568bf77b6c94
lambk <kyle.lamb@hotmail.co.nz>
2018-04-03 20:43:23 +1200

    Added documentation to the controller

--- app/controller.js
+++ app/controller.js
@@ -2,7 +2,7 @@ const model = require('./model.js');
 
 let root = ''
 exports.setRoot = function(rootP) {
-  root = rootP;
+  root = rootP; //Used to provide an absolute path to jade files
 }
 
 exports.loadMainPage = function(req, res) {
@@ -14,76 +14,111 @@ exports.loadInvalidPage = function(req, res) {
 };
 
 exports.loadAirportPage = function(req, res) {
-  let data = undefined;
+  let data = undefined; //Start with a function-scope empty object
+  //Grabbing airport data from the cache or api
   fetchAirportCache(req.params.icao.toUpperCase())
-  .then(handleAirportCache)
-  .then(function(airport) {
-    data = {
-      title: airport.icao + ' - Airport Lookup',
-      icao: airport.icao + ' - ' + airport.name,
-      latitude: airport.latitude.decimal,
-      longitude: airport.longitude.decimal,
-      city: airport.city,
-      country: airport.country,
-      timezone: airport.timezone.tzid + ' (' + airport.timezone.gmt + ' GMT)',
-      status: airport.status
-    }
-    return airport.icao;
-  })
-  .then(fetchMetarCache)
-  .then(handleMetarCache)
-  .then(function(metar) {
-    data.metar = metar.metar;
-    res.render(root + '/views/airport.jade', data);
-  })
-  .catch(function(exception) {
-    if (exception.type == 'redirect') {
-      res.redirect(exception.data);
-    } else if (exception.type == 'error') {
-      console.log('ERROR: [' + exception.code + '] ' + exception.msg);
-      res.status(exception.code).send(exception.msg);
-    }
-  });
+    .then(handleAirportCache)
+    .then(function(airport) {
+      //Add airport data to the data object
+      data = {
+        title: airport.icao + ' - Airport Lookup',
+        icao: airport.icao + ' - ' + airport.name,
+        latitude: airport.latitude.decimal,
+        longitude: airport.longitude.decimal,
+        city: airport.city,
+        country: airport.country,
+        timezone: airport.timezone.tzid + ' (' + airport.timezone.gmt + ' GMT)',
+        status: airport.status
+      }
+      return airport.icao;
+    })
+    //Grabbing metar data from the cache or api
+    .then(fetchMetarCache)
+    .then(handleMetarCache)
+    .then(function(metar) {
+      //Add metar data to the data object
+      data.metar = metar.metar;
+      //Pass the full data and render the page
+      res.render(root + '/views/airport.jade', data);
+    })
+    .catch(function(exception) {
+      if (exception.type == 'redirect') {
+        res.redirect(exception.data); //Redirect to the provided endpoint
+      } else if (exception.type == 'error') {
+        console.log('ERROR: [' + exception.code + '] ' + exception.msg);
+        //Send the status code & message back to the client
+        res.status(exception.code).send(exception.msg);
+      }
+    });
 };
 
+/*
+ * Retreives the airport data from the database if it exists
+ */
 function fetchAirportCache(icao) {
   return new Promise(function(resolve, reject) {
     model.getAirportCache(icao, function(data) {
-      resolve({icao: icao, data: data});
+      resolve({
+        icao: icao,
+        data: data
+      });
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg}); //In the case that the server couldn't read from the database
+      //In the case that the server couldn't read from the database
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     });
   });
 }
 
+/*
+ * Retrieves the metar data from the database if it exists
+ */
 function fetchMetarCache(icao) {
   return new Promise(function(resolve, reject) {
     model.getMetarCache(icao, function(data) {
-      resolve({icao: icao, data: data});
+      resolve({
+        icao: icao,
+        data: data
+      });
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg}); //In the case that the server couldn't read from the database
+      //In the case that the server couldn't read from the database
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     })
   })
 }
 
+/*
+ * Analyses the database airport data from the database to determine whether an api call, and/or cache update is needed
+ * Cases include:
+ *  -No cache data -> Call api and cache the data in the database
+ *  -Expired cache data -> Delete cache data, recall api and add the updated data to the database
+ *  -Valid cache data -> Skip straight to loading the page using the cached data
+ */
 function handleAirportCache(result) {
   return new Promise(function(resolve, reject) {
     let icao = result.icao;
     let rows = result.data;
-    if (rows == undefined || rows.length == 0) {
+    if (rows == undefined || rows.length == 0) { //No cache data
       callAirportApi(icao).then(addAirportCache).then(function(data) {
         resolve(data);
       }).catch(function(exception) {
         reject(exception);
       });
     } else {
-      if (rows[0].current_time > rows[0].expiry) {
+      if (rows[0].current_time > rows[0].expiry) { //Expired cache data
         deleteAirportCache(rows[0].icao).then(callAirportApi).then(addAirportCache).then(function(data) {
           resolve(data);
         }).catch(function(exception) {
           reject(exception);
         });
-      } else {
+      } else { //Valid cache data
         let formatted_data = {
           icao: rows[0].icao,
           name: rows[0].title,
@@ -107,24 +142,31 @@ function handleAirportCache(result) {
   });
 }
 
+/*
+ * Analyses the database metar data from the database to determine whether an api call, and/or cache update is needed
+ * Cases include:
+ *  -No cache data -> Call api and cache the data in the database
+ *  -Expired cache data -> Delete cache data, recall api and add the updated data to the database
+ *  -Valid cache data -> Skip straight to loading the page using the cached data
+ */
 function handleMetarCache(result) {
   return new Promise(function(resolve, reject) {
     let icao = result.icao;
     let rows = result.data;
-    if (rows == undefined || rows.length == 0) {
+    if (rows == undefined || rows.length == 0) { //No cache data
       callMetarApi(icao).then(addMetarCache).then(function(data) {
         resolve(data);
       }).catch(function(exception) {
         reject(exception);
       });
     } else {
-      if (rows[0].current_time > rows[0].expiry) {
+      if (rows[0].current_time > rows[0].expiry) { //Expired cache data
         deleteMetarCache(rows[0].icao).then(callMetarApi).then(addMetarCache).then(function(data) {
           resolve(data);
         }).catch(function(exception) {
           reject(exception);
         });
-      } else {
+      } else { //Valid cache data
         let formatted_data = {
           icao: rows[0].icao,
           metar: rows[0].metar
@@ -135,50 +177,90 @@ function handleMetarCache(result) {
   });
 }
 
+/*
+ * Calls the model function to delete the cached airport record from the database
+ */
 function deleteAirportCache(icao) {
   return new Promise(function(resolve, reject) {
     model.deleteAirportCache(icao, function(result) {
       resolve(icao);
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg});
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     });
   });
 }
 
+/*
+ * Calls the model function to delete the cached metar record from the database
+ */
 function deleteMetarCache(icao) {
   return new Promise(function(resolve, reject) {
     model.deleteMetarCache(icao, function(result) {
       resolve(icao);
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg});
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     });
   });
 }
 
+/*
+ * Calls the model function to call the airport api
+ */
 function callAirportApi(icao) {
   return new Promise(function(resolve, reject) {
     model.callAirportApi(icao, function(result) {
       resolve(result);
     }, function(code, msg) {
-      reject('Error calling airport api');
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     }, function() {
-      reject({ type: 'redirect', data: '/invalid-airport' });
+      reject({
+        type: 'redirect',
+        data: '/invalid-airport'
+      });
     });
   });
 }
 
+/*
+ * Calls the model function to call the metar api
+ */
 function callMetarApi(icao) {
   return new Promise(function(resolve, reject) {
     model.callMetarApi(icao, function(result) {
-      resolve({icao: icao, metar: result});
+      resolve({
+        icao: icao,
+        metar: result
+      });
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg});
-    }, function() {
-      reject({type: 'redirect', data: '/invalid-airport'});
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
+    }, function() { //On non-existant icao
+      reject({
+        type: 'redirect',
+        data: '/invalid-airport'
+      });
     });
   });
 }
 
+/*
+ * Calls the model function to add the provided data as a new airport cache record
+ */
 function addAirportCache(api_data) {
   let sql_data = [
     api_data.icao, api_data.name, api_data.latitude.decimal, api_data.longitude.decimal,
@@ -188,15 +270,23 @@ function addAirportCache(api_data) {
     model.addAirportCache(sql_data, function() {
       resolve(api_data);
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg});
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     });
   });
 }
 
+/*
+ * Calls the model function to add the provided data as a new metar cache record
+ * Except when the metar record is an "METAR Unavailable" record, in which case
+ * execution skips the model call
+ */
 function addMetarCache(api_data) {
   /* Prevents adding the metar cache if the metar is unavailable
-   * Subsequent page loads will call the api incase the data is now available
-   */
+   * Subsequent page loads will call the api incase the data is now available */
   if (api_data.metar.endsWith("METAR Currently Unavailable")) {
     return api_data;
   }
@@ -207,7 +297,11 @@ function addMetarCache(api_data) {
     model.addMetarCache(sql_data, function() {
       resolve(api_data);
     }, function(code, msg) {
-      reject({type: 'error', code: code, msg: msg});
+      reject({
+        type: 'error',
+        code: code,
+        msg: msg
+      });
     });
   });
 }
