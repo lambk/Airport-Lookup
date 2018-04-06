e178f2c78e2c55430f7dec3ed3e2c782d59c362e
lambk <kyle.lamb@hotmail.co.nz>
2018-04-06 17:40:30 +1200

    Added express-sslify to enforce https

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
+const enforce = require('express-sslify');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
+app.user(enforce.HTTPS({trustProtoHeader : true}));
 
 require('./app/routes.js')(app, __dirname);
 
