08e4c335676261cee51a3005f6f0aadef1c19417
lambk <kyle.lamb@hotmail.co.nz>
2018-05-17 23:57:04 +1200

    Added database logging for airport and metar cache/api actions.

---
+++ app/logger.js
@@ -0,0 +1,16 @@
+const db = require('../db');
+
+exports.TYPE = {
+  AIRPORT: 'Airport',
+  METAR: 'Metar',
+  API: 'API',
+  ERROR: 'Error'
+};
+
+exports.log = function(type, message) {
+  console.log(type + ': ' + message); //Print the log to console
+  //Add the log to the database
+  db.get_pool().query('INSERT INTO logs(type, message) VALUES(?,?)', [type, message], (err, result) => {
+    if (err) console.log(err);
+  });
+};
--- app/model.js
+++ app/model.js
@@ -1,10 +1,11 @@
 const db = require('../db');
 const request = require('request');
+const logger = require('./logger.js');
 
 exports.getAirportCache = function(icao, done, failure) {
   db.get_pool().query('SELECT *, NOW() AS \'current_time\' FROM airport_cache WHERE icao=?', icao, function(err, rows) {
     if (err) return failure(500, 'Error reading from airport cache');
-    if (rows.length > 0) console.log(`[CACHE] Airport data fetched for ${icao}`);
+    if (rows.length > 0) logger.log(logger.TYPE.AIRPORT, `Airport cache fetched for ${icao}`);
     return done(rows);
   });
 };
@@ -12,7 +13,7 @@ exports.getAirportCache = function(icao, done, failure) {
 exports.getMetarCache = function(icao, done, failure) {
   db.get_pool().query('SELECT *, NOW() AS \'current_time\' FROM metar_cache WHERE icao=?', icao, function(err, rows) {
     if (err) return failure(500, 'Error reading from metar cache');
-    if (rows.length > 0) console.log(`[CACHE] Metar data fetched for ${icao}`);
+    if (rows.length > 0) logger.log(logger.TYPE.METAR, `Metar cache fetched for ${icao}`);
     return done(rows);
   });
 };
@@ -20,7 +21,7 @@ exports.getMetarCache = function(icao, done, failure) {
 exports.deleteAirportCache = function(icao, done, failure) {
   db.get_pool().query('DELETE FROM airport_cache WHERE icao=?', icao, function(err, result) {
     if (err) return failure(500, 'Error deleting from airport cache');
-    console.log(`[CACHE] Airport data deleted for ${icao}`);
+    logger.log(logger.TYPE.AIRPORT, `Airport cache deleted for ${icao}`);
     return done(result);
   });
 };
@@ -28,7 +29,7 @@ exports.deleteAirportCache = function(icao, done, failure) {
 exports.deleteMetarCache = function(icao, done, failure) {
   db.get_pool().query('DELETE FROM metar_cache WHERE icao=?', icao, function(err, result) {
     if (err) return failure(500, 'Error deleting from metar cache');
-    console.log(`[CACHE] Metar data deleted for ${icao}`);
+    logger.log(logger.TYPE.METAR, `Metar cache deleted for ${icao}`);
     return done(result);
   });
 };
@@ -36,9 +37,8 @@ exports.deleteMetarCache = function(icao, done, failure) {
 exports.addAirportCache = function(data, done, failure) {
   db.get_pool().query('INSERT INTO airport_cache (icao, title, latitude, longitude, elevation, magnetic, city, state, country, timezone_tzid, timezone_gmt, `status`, `usage`) \
   VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', data, function(err, result) {
-    //if (err) return failure(500, 'Error inserting into airport cache');
-    if (err) return failure(500, err);
-    console.log(`[CACHE] Airport data added for ${data[0]}`);
+    if (err) return failure(500, 'Error inserting into airport cache');
+    logger.log(logger.TYPE.AIRPORT, `[CACHE] Airport cache added for ${data[0]}`);
     return done(result);
   });
 };
@@ -46,13 +46,13 @@ exports.addAirportCache = function(data, done, failure) {
 exports.addMetarCache = function(data, done, failure) {
   db.get_pool().query('INSERT INTO metar_cache (icao, metar) VALUES (?,?)', data, function(err, result) {
     if (err) return failure(500, 'Error inserting into metar cache');
-    console.log(`[CACHE] Metar data added for ${data[0]}`);
+    logger.log(logger.TYPE.METAR, `[CACHE] Metar cache added for ${data[0]}`);
     return done(result);
   });
 };
 
 exports.callAirportApi = function(icao, done, failure, redirect) {
-  console.log(`[API] Fetching Airport Data for ${icao} from CheckWx`);
+  logger.log(logger.TYPE.API, `Fetching Airport Data for ${icao} from CheckWx`);
   let options = {
     url: 'https://api.checkwx.com/station/' + icao,
     headers: {
@@ -61,25 +61,25 @@ exports.callAirportApi = function(icao, done, failure, redirect) {
     'Content-Type': 'application/json'
   }
   request(options, function(err, response, body) {
-    console.log(`[API] Response code: ${response.statusCode}`);
+    logger.log(logger.TYPE.API, `Response code: ${response.statusCode}`);
     if (response.statusCode == 200) {
       try {
         let data = JSON.parse(response.body);
         if (data.data[0].icao == undefined) return redirect();
         done(data.data[0]);
       } catch(e) {
-        console.log(e);
+        logger.log(logger.TYPE.ERROR, 'Error converting API response to JSON');
         failure(500, 'Internal server error');
       }
     } else {
-      console.log(err);
+      logger.log(logger.TYPE.ERROR, `${response.statusCode} response from API`);
       failure(500, 'Internal server error');
     }
   });
 }
 
 exports.callMetarApi = function(icao, done, failure, redirect) {
-  console.log(`[API] Fetching metar for ${icao} from CheckWx`);
+  logger.log(logger.TYPE.API, `Fetching metar for ${icao} from CheckWx`);
   let options = {
     url: 'https://api.checkwx.com/metar/' + icao,
     headers: {
@@ -87,18 +87,18 @@ exports.callMetarApi = function(icao, done, failure, redirect) {
     }
   }
   request(options, function(err, response, body) {
-    console.log(`[API] Response code: ${response.statusCode}`);
+    logger.log(logger.TYPE.API, `Response code: ${response.statusCode}`);
     if (response.statusCode == 200) {
       try {
         let data = JSON.parse(response.body);
         if (data.data[0].endsWith('Invalid Station ICAO')) return redirect();
         done(data.data[0]);
       } catch(e) {
-        console.log(e);
+        logger.log(logger.TYPE.ERROR, 'Error converting API response to JSON');
         failure(500, 'Internal server error');
       }
     } else {
-      console.log(err);
+      logger.log(logger.TYPE.ERROR, `${response.statusCode} response from API`);
       failure(500, 'Internal server error');
     }
   });
