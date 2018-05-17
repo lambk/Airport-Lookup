8829048f2100005861b548cf5e00351000cf3fef
lambk <kyle.lamb@hotmail.co.nz>
2018-05-17 23:58:38 +1200

    Corrected error message for insuccessful addition of a log to the database

--- app/logger.js
+++ app/logger.js
@@ -11,6 +11,6 @@ exports.log = function(type, message) {
   console.log(type + ': ' + message); //Print the log to console
   //Add the log to the database
   db.get_pool().query('INSERT INTO logs(type, message) VALUES(?,?)', [type, message], (err, result) => {
-    if (err) console.log(err);
+    if (err) console.log('Error adding log to database');
   });
 };
